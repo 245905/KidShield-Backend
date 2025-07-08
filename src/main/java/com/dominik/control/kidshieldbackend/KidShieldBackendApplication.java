12598ae1cbcc39af3efd2559f9902b01f7876725
Your Name <you@example.com>
2025-07-08 17:00:16 +0200

    Initial project files

---
+++ src/main/java/com/dominik/control/kidshieldbackend/KidShieldBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dominik.control.kidshieldbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidShieldBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidShieldBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dominik/control/kidshieldbackend/KidShieldBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominik.control.kidshieldbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidShieldBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
