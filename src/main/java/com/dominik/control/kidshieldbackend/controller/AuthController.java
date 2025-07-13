a21496c12ff6b9d540503769427c28226f9047ba
Your Name <you@example.com>
2025-07-13 18:22:45 +0200

    Added login endpoint

--- src/main/java/com/dominik/control/kidshieldbackend/controller/AuthController.java
+++ src/main/java/com/dominik/control/kidshieldbackend/controller/AuthController.java
@@ -1,11 +1,35 @@
 package com.dominik.control.kidshieldbackend.controller;
 
+import com.dominik.control.kidshieldbackend.dto.AuthResponse;
+import com.dominik.control.kidshieldbackend.dto.LoginRequest;
+import com.dominik.control.kidshieldbackend.service.AuthenticationService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
-@AllArgsConstructor
+@RequestMapping(path = "/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest){
+        UserDetails userDetails = authenticationService.authenticate(
+                loginRequest.getEmail(),
+                loginRequest.getPassword()
+        );
+        String tokenValue = authenticationService.generateToken(userDetails);
+        AuthResponse authResponse = AuthResponse.builder()
+                .token(tokenValue)
+                .expiresIn(86400)
+                .build();
+        return ResponseEntity.ok(authResponse);
+    }
 }
